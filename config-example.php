144c42a3b13693d071a54a7bee739eda8b39f5aa
grazingllama <elia@sandors.de>
2024-10-08 14:21:19 +0200

    removed unused files

--- config-example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("DATABASE_HOSTNAME", "localhost");
-define("DATABASE_USERNAME", "db_username");
-define("DATABASE_PASSWORD", "db_password");
-define("DATABASE_NAME", "db_name");
-
-?>
